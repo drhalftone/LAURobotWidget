ef0a91dcb6c56c7afd2f32b2c98832d0a60bd56c
AustinL071 <Austin.Langton@uky.edu>
2017-11-10 13:48:43 -0500

    added austincolorcamwidget

---
+++ austincolorcamwidget.cpp
@@ -0,0 +1,6 @@
+#include "austincolorcamwidget.h"
+
+austincolorcamwidget::austincolorcamwidget()
+{
+
+}
---
+++ austincolorcamwidget.h
@@ -0,0 +1,11 @@
+#ifndef AUSTINCOLORCAMWIDGET_H
+#define AUSTINCOLORCAMWIDGET_H
+
+
+class austincolorcamwidget
+{
+public:
+    austincolorcamwidget();
+};
+
+#endif // AUSTINCOLORCAMWIDGET_H
